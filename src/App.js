6c555669bcf301ed448d2914bb7e831e881b934a
sergeycin <67867645+sergeycin@users.noreply.github.com>
2021-11-08 22:01:56 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React,{useState} from "react";
+
+function computeInitialCounter(){
+  console.log('Some calculation')
+  return Math.trunc(Math.random()* 20)
+}
 
 function App() {
+  const [counter,setCounter] = useState( () => {
+   return computeInitialCounter()
+  });
+
+  const [] = useState ({
+    title: 'Счетчик',
+    date: Date.now()
+  })
+  
+  function increment () {
+    setCounter((prevCounter) =>{
+      return prevCounter + 1; 
+    })
+    // setCounter(prev => prev +10)
+  }
   return (
-    <div className="App">
-<h2></h2>
+    <div >
+<h2>Счетчик: {counter}</h2>
+<button onClick={increment} className="btn"></button>
     </div>
   );
 }
