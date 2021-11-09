185a3f61f7a617b9aee38d521fc3c260a792e6bf
sergeycin <67867645+sergeycin@users.noreply.github.com>
2021-11-09 18:08:27 +0300

    da

--- src/App.js
+++ src/App.js
@@ -1,36 +1,19 @@
-import React,{useState, useEffect} from "react";
+import React,{useState, useEffect,useRef} from "react";
+
 
 
 function App() {
 
-  const [type,setType] = useState('users')
-  const [pos,setPos] = useState({
-    x:0, y:0
-  })
+  // const [renderCount,set] = useState
+  const [value, setValue] = useState('initial');
 
-  const mouseMoveHandler = event =>{
-    setPos({
-      x: event.clientX,
-      y: event.clientY
-    })
-  }
-
-  useEffect(() =>{
-  window.addEventListener('mousemove', mouseMoveHandler)
-  return () => {
-    window.removeEventListener('mousemove', mouseMoveHandler);
-  }
-  }, [type])
-
+ 
   return (
     <div >
-      <h1>Ресурс: {type}</h1>
-      <button onClick={() => setType('users')}>пользователи</button>
-      <button onClick={() => setType('todo')}>ToDO</button>
-      <button onClick={() => setType('posts')}>Посты</button>
-      <pre>{JSON.stringify(pos, null, 2)}</pre>
+
     </div>
   );
 }
 
 export default App;
+ 
